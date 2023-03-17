aa7a7157640aecb09a6e26fac7314703d4aa9d4a
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-17 15:01:42 +0100

    add track and instruments for work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,19 @@
-alert("funziona");
+/* 
+L'utente clicca su un bottone
+che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe
+Quando l'utente clicca su una cella, la cella cliccata si colora di azzurro
+ed emetto un messaggio in console con il numero della cella cliccata.
+*/
+
+/* 
+ STRUMENTI USATI
+ let/const
+ console.log()
+ cicle For[]
+ function
+ getElementById
+ append?
+*/
 
