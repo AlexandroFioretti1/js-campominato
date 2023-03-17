38ea6129fd85c73997e2eacf4ee4723f3844cf59
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-17 16:43:40 +0100

    add element "table whit tr and td"  errore riga 46/50

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
-/* 
-L'utente clicca su un bottone
+/* L'utente clicca su un bottone
 che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe
 Quando l'utente clicca su una cella, la cella cliccata si colora di azzurro
-ed emetto un messaggio in console con il numero della cella cliccata.
-*/
+ed emetto un messaggio in console con il numero della cella cliccata. */
 
 /* 
  STRUMENTI USATI
@@ -13,7 +11,65 @@ ed emetto un messaggio in console con il numero della cella cliccata.
  console.log()
  cicle For[]
  function
- getElementById
- append?
+ addElementById
+ addEventListener
+ createElement
 */
 
+// Seleziono l'elemento HTML del bottone PLAY
+const playButton = document.getElementById("play_button");
+
+// Aggiungo un listener per l'evento 'click' sul bottone
+playButton.addEventListener("click", function () {
+
+  // Seleziono l'elemento HTML dove inserire la griglia di gioco
+  const containerGame = document.getElementById("container_game");
+
+  // Inizializzo una variabile per il numero della cella corrente
+  let cellNumber = 1;
+
+  // Creo una tabella HTML con 10 righe e 10 colonne
+  const table = document.createElement("table");
+  for (let i = 0; i < 10; i++) {
+
+    // Creo una riga della tabella
+    const row = document.createElement("tr");
+    for (let j = 0; j < 10; j++) {
+
+      // Creo una cella della riga
+      const cell = document.createElement("td");
+
+      // Imposto il numero della cella come contenuto della cella
+      cell.textContent = cellNumber;
+
+      // Aggiungo un listener per l'evento 'click' sulla cella
+      cell.addEventListener("click", function () {
+        /* 
+        ho provato a usare toggle al posto di click per far tornare normale la casella in caso di doppio click ma non funziona all'intero del codice questo è come l'ho scritto :
+        cell.addEventListener("toggle", function () {
+        */
+
+        // Coloro la cella di blu chiaro
+        this.style.backgroundColor = "lightblue";
+
+        // Stampo il numero della cella nella console
+        console.log("Hai cliccato nella casella con il Numero " + this.textContent);
+
+        // Aggiorno il numero della cella corrente
+        cellNumber++;
+      });
+
+      // Aggiungo la cella alla riga
+      row.appendChild(cell);
+
+      // Aggiorno il numero della cella corrente
+      cellNumber++;
+    }
+
+    // Aggiungo la riga alla tabella
+    table.appendChild(row);
+  }
+
+  // Inserisco la tabella nel container di gioco
+  containerGame.appendChild(table);
+});
